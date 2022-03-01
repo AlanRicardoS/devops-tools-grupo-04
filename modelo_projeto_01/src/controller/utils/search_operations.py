1cccd10077338c9eefa21d82038b1f1ab06890e5
Julia Paiva <juliavpaiva@outlook.com>
2022-03-01 16:59:06 -0300

    Creating base search_operations file

---
+++ modelo_projeto_01/src/controller/utils/search_operations.py
@@ -0,0 +1,11 @@
+import pandas
+
+def group_by():
+    # TODO: implement function following:
+    # https://github.com/AlanRicardoS/devops-tools-grupo-04/issues/14
+    return None
+
+def filter_by():
+    # TODO: implement function following:
+    # https://github.com/AlanRicardoS/devops-tools-grupo-04/issues/15
+    return None
