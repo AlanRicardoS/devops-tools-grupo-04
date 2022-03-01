1d8d94d815be6a086c76458b63e3d96f2618b302
Julia Paiva <juliavpaiva@outlook.com>
2022-03-01 18:44:29 -0300

    Implementing filter function

--- modelo_projeto_01/src/controller/utils/search_operations.py
+++ modelo_projeto_01/src/controller/utils/search_operations.py
@@ -1,11 +1,28 @@
-import pandas
+import pandas as pd
+
+from exceptions import FilterOperationUnavailable
+
+CAR_VALUE_COLUMN_NAME = 'car_value'
+CAR_MAKE_COLUMN_NAME = 'car_make'
+CITY_COLUMN_NAME = 'city'
 
 def group_by():
     # TODO: implement function following:
     # https://github.com/AlanRicardoS/devops-tools-grupo-04/issues/14
     return None
 
-def filter_by():
-    # TODO: implement function following:
-    # https://github.com/AlanRicardoS/devops-tools-grupo-04/issues/15
-    return None
+def filter_by(dataset, reference_column, filter_value):
+
+    if (reference_column == CAR_MAKE_COLUMN_NAME):
+        filtered_df = dataset.drop(dataset[dataset.car_make != filter_value].index)
+        
+    elif (reference_column == CITY_COLUMN_NAME):
+        filtered_df = dataset.drop(dataset[dataset.city != filter_value].index)
+    
+    else:
+        raise FilterOperationUnavailable("Filter not available for requested column", reference_column)
+
+    grouped_df = filtered_df.groupby([reference_column]).mean()
+    car_value_mean = grouped_df[[CAR_VALUE_COLUMN_NAME]]
+
+    return car_value_mean
