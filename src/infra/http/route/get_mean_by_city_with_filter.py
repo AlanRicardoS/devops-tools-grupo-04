c6bfe127028802653494e0a49fe4c73136f058b0
AlanRicardoS <41715731@mackenzista.com.br>
2022-03-04 23:56:26 -0300

    adicionando busca por cidade

--- src/infra/http/route/get_mean_by_city_with_filter.py
+++ src/infra/http/route/get_mean_by_city_with_filter.py
@@ -1,18 +1,18 @@
 from flask import jsonify, request
 from src.infra.utility.http import Http
 import src.infra.http.auth as auth
-import src.controller.get_mean_by_carmake_with_filter as get_mean_by_carmake_with_filter
+import src.controller.get_mean_by_city_with_filter as get_mean_by_city_with_filter
 import flask
 
 
 def route(app: flask.app.Flask):
-    @app.route('/carmake/filter', methods=['GET'])
+    @app.route('/city/filter', methods=['GET'])
     @auth.requires_auth
-    def request_get_carmake_with_filter():
+    def request_get_city_with_filter():
         try:
 
-            response = get_mean_by_carmake_with_filter.get_mean_by_carmake_with_filter(
-                request.headers["carmake"])
+            response = get_mean_by_city_with_filter.get_mean_by_city_with_filter(
+                request.headers["city"])
 
             return response, 200
         except Exception as err:
