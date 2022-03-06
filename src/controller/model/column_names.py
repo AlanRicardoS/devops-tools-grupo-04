5475b0faf8ed565a6acf26d18b742bc811a27f9a
Julia Paiva <juliavpaiva@outlook.com>
2022-03-05 21:18:56 -0300

    Creating ColumnNames Enum and Refactoring Search Operations

---
+++ src/controller/model/column_names.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ColumnNames(Enum):
+    CAR_VALUE = 'car_value'
+    CAR_MAKE = 'car_make'
+    CITY = 'city'
--- src/controller/utils/search_operations.py
+++ src/controller/utils/search_operations.py
@@ -1,36 +1,35 @@
 import pandas as pd
 from src.controller.utils.exceptions import FilterOperationUnavailable
-
-CAR_VALUE_COLUMN_NAME = 'car_value'
-CAR_MAKE_COLUMN_NAME = 'car_make'
-CITY_COLUMN_NAME = 'city'
-
+from src.controller.model.column_names import ColumnNames
 
 def group_by(dataset, group_value):
     try:
         grouped = dataset.groupby(group_value).mean()
-        car_value_mean = grouped[[CAR_VALUE_COLUMN_NAME]]
+        car_value_mean = grouped[[ColumnNames.CAR_VALUE.value]]
     except:
         raise FilterOperationUnavailable(
             "Could not group by dataset using " + group_value)
+
     return car_value_mean
 
 
 def filter_by(dataset, reference_column, filter_value):
 
-    if (reference_column == CAR_MAKE_COLUMN_NAME):
-        filtered_dataset = dataset.drop(
-            dataset[dataset.car_make != filter_value].index)
+    filtered_dataset =_filter_dataset(dataset, reference_column, filter_value)
 
-    elif (reference_column == CITY_COLUMN_NAME):
-        filtered_dataset = dataset.drop(
-            dataset[dataset.city != filter_value].index)
+    grouped_dataset = group_by(filtered_dataset, reference_column)
+    car_value_mean = grouped_dataset[[ColumnNames.CAR_VALUE.value]]
+
+    return car_value_mean
+
+def _filter_dataset(dataset, reference_column, filter_value):
+
+    if (reference_column == ColumnNames.CAR_MAKE.value):
+        return dataset.drop(dataset[dataset.car_make != filter_value].index)
+
+    elif (reference_column == ColumnNames.CITY.value):
+        return dataset.drop(dataset[dataset.city != filter_value].index)
 
     else:
         raise FilterOperationUnavailable(
             "Filter not available for requested column", reference_column)
-
-    grouped_dataset = group_by(filtered_dataset, reference_column)
-    car_value_mean = grouped_dataset[[CAR_VALUE_COLUMN_NAME]]
-
-    return car_value_mean
